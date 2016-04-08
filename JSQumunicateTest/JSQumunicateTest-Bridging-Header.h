a8e16a5e970f302fe3374719f22b4e2b39db3297
fans3210 <liyaodong2@gmail.com>
2016-04-08 14:24:18 +0800

    commit

--- JSQumunicateTest/JSQumunicateTest-Bridging-Header.h
+++ JSQumunicateTest/JSQumunicateTest-Bridging-Header.h
@@ -2,7 +2,7 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import <Quickblox/Quickblox.h>
+
 #import <SVProgressHUD.h>
 #import "QMServices.h"
 #import "TWMessageBarManager.h"
